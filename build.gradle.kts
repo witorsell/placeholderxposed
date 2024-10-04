3029660ad42f6af090a889c6ae71c684b6c71fb6
Pun Butrach <pun.butrach@gmail.com>
2024-10-04 21:48:24 +0700

    build: Remove JCenter as repository source

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,6 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        // We need jcenter for the Xposed API...
-        jcenter()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.0.1")
@@ -20,8 +18,6 @@ allprojects {
         google()
         mavenCentral()
         maven(url = "https://api.xposed.info/")
-        // See comment in buildscript repos
-        jcenter()
     }
 }
 
