02979e1526c68d422d1598d7d2b54f7d3e484d03
pylixonly <82711525+pylixonly@users.noreply.github.com>
2024-11-13 12:14:24 +0800

    chore: bump version to 0.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.pyoncord.xposed"
         minSdk = 24
         targetSdk = 33
-        versionCode = 204
-        versionName = "0.2.4"
+        versionCode = 205
+        versionName = "0.2.5"
     }
 
     buildTypes {
