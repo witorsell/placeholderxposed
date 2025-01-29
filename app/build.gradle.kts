eb5b2be40a00fa111ffa50849aae948cd04373e0
pylix <82711525+pylixonly@users.noreply.github.com>
2025-01-30 00:24:42 +0800

    chore(app): Bump to 0.2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.pyoncord.xposed"
         minSdk = 24
         targetSdk = 33
-        versionCode = 205
-        versionName = "0.2.5"
+        versionCode = 206
+        versionName = "0.2.6"
     }
 
     buildTypes {
