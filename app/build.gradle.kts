78ac59493e6f68ae02b68e4e64dccd5ec06e294b
pylixonly <82711525+pylixonly@users.noreply.github.com>
2025-02-27 20:15:58 +0800

    chore: bump targetSdk to 35 and kotlin to 2.1.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    kotlin("plugin.serialization") version "1.8.21"
+    kotlin("plugin.serialization") version "2.1.10"
 }
 
 android {
     namespace = "io.github.pyoncord.xposed"
-    compileSdk = 33
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "io.github.pyoncord.xposed"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 35
         versionCode = 206
         versionName = "0.2.6"
     }
@@ -40,9 +40,10 @@ android {
 
 dependencies {
     compileOnly("de.robv.android.xposed:api:82")
-    implementation("androidx.core:core:1.10.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
-    implementation("io.ktor:ktor-client-core:2.3.0")
-    implementation("io.ktor:ktor-client-cio:2.3.0")
+
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
+    implementation("io.ktor:ktor-client-core:3.1.1")
+    implementation("io.ktor:ktor-client-cio:3.1.1")
 }
