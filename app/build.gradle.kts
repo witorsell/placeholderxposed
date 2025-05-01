bb7ba2eae320fd15f398e4e209710a9cebdd9167
cocobo1 <cocobotheone@gmail.com>
2025-05-01 18:10:56 +0100

    Fixed old android version support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
