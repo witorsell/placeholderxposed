448ed645dc2b36c144212438aa3ca98e09a38b04
PalmDevs <git@palmdevs.me>
2025-05-06 15:21:21 +0700

    chore: bump dependencies and version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,9 +11,9 @@ android {
     defaultConfig {
         applicationId = "io.github.revenge.xposed"
         minSdk = 24
-        targetSdk = 33
-        versionCode = 1101
-        versionName = "1.1.1"
+        targetSdk = 35
+        versionCode = 1201
+        versionName = "1.2.1"
     }
 
     buildTypes {
@@ -41,7 +41,7 @@ android {
 dependencies {
     compileOnly("de.robv.android.xposed:api:82")
 
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
     implementation("io.ktor:ktor-client-core:3.1.1")
