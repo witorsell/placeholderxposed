ecae49a23d0c5626177f498ebfd5fc6cb82ff933
cocobo1 <cocobotheone@gmail.com>
2025-05-22 19:27:01 +0300

    bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cocobo1.pupu.xposed"
         minSdk = 22
         targetSdk = 36
-        versionCode = 1110
-        versionName = "1.1.1"
+        versionCode = 1120
+        versionName = "1.1.2"
     }
 
     buildTypes {
