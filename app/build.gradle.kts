bbccf45eb07a5da7541a310726405a55d00b4d62
kmmiio99o <164557248+kmmiio99o@users.noreply.github.com>
2025-10-24 22:51:04 +0200

    frogor to bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ShiggyXposed.xposed"
         minSdk = 22
         targetSdk = 36
-        versionCode = 1040
-        versionName = "1.0.4"
+        versionCode = 1050
+        versionName = "1.0.5"
     }
 
     buildTypes {
