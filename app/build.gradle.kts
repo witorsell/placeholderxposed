8b36ea51f68d44f96e932b805ef6018fb6e92d13
kmmiio99o <164557248+kmmiio99o@users.noreply.github.com>
2026-01-31 20:28:57 +0100

    chore: bump to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ShiggyXposed.xposed"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1250
-        versionName = "1.2.5"
+        versionCode = 1300
+        versionName = "1.3.0"
     }
 
     buildTypes {
